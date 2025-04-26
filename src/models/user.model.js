19feeeb7fc641528c8fef478a30cd08aa7d794f6
udayrana428 <udayrana428@gmail.com>
2025-04-26 15:55:23 +0530

    changed environment variable

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -74,7 +74,7 @@ userSchema.methods.generateRefreshToken = async function () {
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRTY,
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
   );
 };
